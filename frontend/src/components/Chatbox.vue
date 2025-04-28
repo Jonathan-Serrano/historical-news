ec3b3fab7469a793899b303b93b2716fa61718d1
Jonathan-Serrano <jonathan.ant.serrano@gmail.com>
2025-04-28 16:54:34 -0500

    add reading lvl button

--- frontend/src/components/Chatbox.vue
+++ frontend/src/components/Chatbox.vue
@@ -4,6 +4,16 @@
       <p><strong>{{ msg.role }}:</strong> {{ msg.content }}</p>
     </div>
     <input v-model="userInput" @keyup.enter="sendMessage" class="border p-2 w-full" />
+    <div class="relative mt-2">
+      <button @click="toggleDropdown" class="border p-2 w-full bg-gray-200">
+        {{ selectedOption }}
+      </button>
+      <ul v-if="dropdownOpen" class="absolute bg-gray-900 border mt-1 w-full">
+        <li v-for="option in options" :key="option" @click="selectOption(option)" class="p-2 hover:bg-gray-100 cursor-pointer">
+          {{ option }}
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -12,6 +22,19 @@ import { ref } from 'vue'
 
 const messages = ref<{ role: string, content: string }[]>([])
 const userInput = ref("")
+const dropdownOpen = ref(false)
+const options = ref(["Easy Read", "Standard Summary", "Deep Dive"])
+const selectedOption = ref("Standard Summary") // Default option set to "B"
+
+const toggleDropdown = () => {
+  dropdownOpen.value = !dropdownOpen.value
+}
+
+const selectOption = (option: string) => {
+  // messages.value.push({ role: "user", content: `Change ${option}` })
+  selectedOption.value = option
+  dropdownOpen.value = false
+}
 
 const sendMessage = async () => {
   messages.value.push({ role: "user", content: userInput.value })
